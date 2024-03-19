efaa453c5cec93ad4979e564b3d110c7be4ac865
CarlosPereira85 <caiqueberlin@gmail>
2024-03-19 14:34:31 +0100

    portfolio_update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './about.css';
 import AboutImg from "../../assets/about.jpeg"
-import CV from '../../assets/Pradeep-Cv.pdf'
+import CV from '../../assets/Lebenslauf.pdf'
 import Info from './Info';
 
 const About = () => {
