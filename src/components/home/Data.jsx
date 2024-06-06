558d40639f7df67726eb555f626500086d343d4d
CarlosPereira85 <caiqueberlin@gmail>
2024-06-06 09:03:33 +0200

    first commit4

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Data = () => {
   return (
     <div className="home__data">
-        <h1 className="home__title">Carlos P
+        <h1 className="home__title">Carlos Pereira
 
         <svg
                   width="36"
